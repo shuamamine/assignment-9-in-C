04f3a991af1dc79977338409f153e84b29038ff1
Tapur <113691557+shuamamine@users.noreply.github.com>
2023-03-30 01:32:30 +0530

    Create Q4.c

---
+++ programs/Q4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+int c=0, n;
+printf("enter limit");
+scanf("%d", &n);
+int a[n];
+printf("enter values in array");
+for(int i = 0; i < n; i++)
+{
+  scanf("%d", a[i]);
+}
+printf("original array");
+ for(int i = 0; i < n; i++)
+{
+   printf("%d", a[i]);
+ }
+ for(int i = 0; i < n; i++)
+{
+    for(int j = i+1; j < n; j++)
+{
+     if(a[i] == a[j])
+     {
+       c++;
+     }
+    }
+ }
+ printf("duplicates in array = %d", c); 
+  return 0;
+}
