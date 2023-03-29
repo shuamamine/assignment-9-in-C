1c33c28752b52577716498abf548bc134a7cb1af
Tapur <113691557+shuamamine@users.noreply.github.com>
2023-03-30 01:15:07 +0530

    Create Q1.c

---
+++ programs/Q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int i,n;
+printf("enter limit");
+scanf("%d", &n);
+int a[n];
+ printf("enter values in array");
+for(int i = 0; i < n; i++)
+{
+  scanf("%d", a[i]);
+}
+ printf("the elements of array are :");
+ for(int i = 0; i < n; i++)
+{
+   printf("%d", a[i]);
+}
+  return 0;
+}
