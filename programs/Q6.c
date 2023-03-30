bf75703f7190936e75fb787ac21bcff2114cf7b6
Tapur <113691557+shuamamine@users.noreply.github.com>
2023-03-31 00:55:51 +0530

    Update Q6.c

--- programs/Q6.c
+++ programs/Q6.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-int m,n,p=0;
+int m,n,p=0,temp;
 printf("enter limit of first array");
 scanf("%d", &n);
-printf("enter limit of first array");
+printf("enter limit of second array");
 scanf("%d", &m);
 int a[n], b[n];
 printf("enter values in array 1 ");
@@ -22,4 +22,29 @@ int c[p];
 for(int i = 0; i < m; i++)
 {
    scanf("%d", b[i]);
+}
+  for(int i = 0; i < m; i++)
+{
+  c[i] = b[i];
+}
+ for(int i = 0; i < n; i++)
+{
+  c[i] = a[i];
+}
+for(int i = 0; i < p; i++)
+{
+  for(int j = 0; j < p - 1; j++)
+  {
+    if(c[j] < c[j+1]){
+      temp = c[j];
+      c[j] = c[j+1];
+      c[j+1] = temp;
+    }
+  }
+}
+for(int i = 0; i < p; i++)
+{
+  printf("%d", c[i]);
+}
+ return 0;
 }
