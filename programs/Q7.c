869ca37c07a32f91b42d7b5e30220dc7630e6dd0
Tapur <113691557+shuamamine@users.noreply.github.com>
2023-03-31 00:59:48 +0530

    Create Q7.c

---
+++ programs/Q7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int n, c=0;
+printf("enter limit of array");
+scanf("%d", &n);
+int a[n];
+printf("enter values in array");
+for(int i = 0; i < n; i++)
+{
+  scanf("%d", a[i]);
+}
+
+ 
