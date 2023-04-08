04db9ad7110578b5f204c14b0c99f9de26ff3785
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-08 23:22:28 +0530

    Update Q7.c

--- programs/Q7.c
+++ programs/Q7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-int n, c=0;
+int n,max=0;min=0, c=0;
 printf("enter limit of array");
 scanf("%d", &n);
 int a[n];
@@ -10,5 +10,12 @@ for(int i = 0; i < n; i++)
 {
   scanf("%d", a[i]);
 }
-
- 
+max  = a[0];
+min  = a[0];
+for(int i = 1; i < n; i++)
+{
+  if(max > a[i])
+    max = a[i];
+  if(min < a[i])
+    min = a[i];
+}
