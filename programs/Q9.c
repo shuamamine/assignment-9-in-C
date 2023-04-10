317d560f2775fc380032deb14b7fcc469246f130
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-10 23:57:20 +0530

    Update Q9.c

--- programs/Q9.c
+++ programs/Q9.c
@@ -12,3 +12,25 @@ for(int i = 0; i < n; i++)
 }
 for(int i = 0; i < n; i++)
 {
+  if(a[i]%2==0)
+  {
+    b[j] = a[i];
+    j++;
+  }
+  else
+  {
+    c[k]=a[i];
+    k++}
+}
+ printf("odd elements are");
+ for(int i = 0; i < k; i++)
+{
+   printf("%d",c[i]);
+ }
+ printf("even elements are");
+ for(int i = 0; i < j; i++)
+{
+   printf("%d",b[i]);
+ }
+  return 0;
+}
