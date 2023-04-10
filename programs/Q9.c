e186f3578f8a1a3a5eca0dd8eb92e708b8b01a54
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-10 23:54:06 +0530

    Update Q9.c

--- programs/Q9.c
+++ programs/Q9.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main()
+{
+int n,i,j=0,k=0;
+printf("enter limit of array");
+scanf("%d", &n);
+int a[n],b[n],c[n];
+printf("enter values in array");
+for(int i = 0; i < n; i++)
+{
+  scanf("%d", a[i]);
+}
+for(int i = 0; i < n; i++)
+{
